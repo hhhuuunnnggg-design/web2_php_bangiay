282bda2b020f5d0d66a0c602c057f8d276fbe34b
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2025-03-14 12:10:07 +0700

    .

--- views/admin/category_index.php
+++ views/admin/category_index.php
@@ -1,10 +1,23 @@
 <h1>Quản lý danh mục</h1>
+<div style="
+    display: flex;
+    justify-content: space-between;">
 <form method="GET" action="/shoeimportsystem/public/index.php" id="searchForm">
     <input type="text" name="search" value="<?php echo htmlspecialchars($search ?? ''); ?>" placeholder="Tìm kiếm danh mục">
     <input type="hidden" name="controller" value="category">
     <input type="hidden" name="action" value="index">
     <button type="submit">Tìm</button>
 </form>
+<?php if ($auth->checkPermission(1, 'add')): ?>
+                    <a href="/shoeimportsystem/public/index.php?controller=category&action=add">
+                        <button type="button" class="btn btn-primary" style="
+    margin-top: 40px;
+    width: 100px;
+    height: 40px;
+">Thêm</button>
+                    </a>
+                <?php endif; ?>
+</div>
 
 <div id="message"></div>
 
@@ -31,11 +44,7 @@
             <td><?php echo $row['MaDM']; ?></td>
             <td><?php echo $row['TenDM']; ?></td>
             <td>
-                <?php if ($auth->checkPermission(1, 'add')): ?>
-                    <a href="/shoeimportsystem/public/index.php?controller=category&action=add">
-                        <button type="button" class="btn btn-primary">Thêm</button>
-                    </a>
-                <?php endif; ?>
+                
                 <?php if ($auth->checkPermission(1, 'edit')): ?>
                     <a href="/shoeimportsystem/public/index.php?controller=category&action=edit&id=<?php echo $row['MaDM']; ?>">
                         <button type="button" class="btn btn-warning">Sửa</button>

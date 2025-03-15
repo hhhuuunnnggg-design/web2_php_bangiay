da68a87ab18ed93002807c3be386f4e22b770fcd
king7514 <phamtanphat4232@gmail.com>
2025-03-16 01:06:52 +0700

    Create ContactController.php

---
+++ controllers/client/ContactController.php
@@ -0,0 +1,10 @@
+<?php
+// controllers/client/ContactController.php
+
+class ContactController {
+    public function index() {
+        // Load view contact.html
+        include __DIR__ . '/../../views/client/layout/contact.html';
+    }
+}
+?>

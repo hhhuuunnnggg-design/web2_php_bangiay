9b61fef4da0d862128a7b9b786bdabda263e97e7
king7514 <phamtanphat4232@gmail.com>
2025-03-16 12:57:17 +0700

    Create ShopdetailController.php

---
+++ controllers/client/ShopdetailController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class ShopdetailController {
+    public function index() {
+        // Load view Shopdetail.php
+        include __DIR__ . '/../../views/client/layout/shopdetail.php';
+    }
+}
+?>

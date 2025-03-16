c184b6d27c2c09a2b9ed0c210ba7382062ecf956
king7514 <phamtanphat4232@gmail.com>
2025-03-16 11:28:49 +0700

    Update contact.php

--- views/client/layout/contact.php
+++ views/client/layout/contact.php
@@ -7,6 +7,7 @@
     <link href="/shoeimportsystem/views/client/layout/css/style.css" rel="stylesheet">
 </head>
 <body>
+    <?php include("header.php"); ?>
     <div class="contact-container">
         <div class="contact-form">
             <h2>Liên hệ</h2>
@@ -38,5 +39,6 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6521522291517!2d106.68005371473215!3d10.761356592334237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1b131139a1%3A0xb36994c92a627a9f!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBTxrAgcGjhuqltIEPDtG5nIG5naOG7hyBUUC5IQ00!5e0!3m2!1svi!2s!4v1678887532845!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
+    <?php include("footer.php"); ?>
 </body>
 </html>
